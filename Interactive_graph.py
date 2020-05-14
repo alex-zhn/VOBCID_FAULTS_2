59bd0f2a2a00fe5cdd9a368f672519c709fac9a2
Alex <alex.si.zhang@gmail.com>
2020-05-14 12:16:55 -0700

    added date range selector

--- Interactive_graph.py
+++ Interactive_graph.py
@@ -12,6 +12,9 @@ import dash_core_components as dcc
 import dash_html_components as html 
 from dash.dependencies import Input, Output
 from datetime import datetime as dt
+from datetime import datetime as dt
+import re
+
 
 import order_data
 import dict_gen
@@ -29,17 +32,17 @@ def data_gen_fc_as_size(df, xfield, yfield, size_scale):
 
 
 df = order_data.import_data("fc1.csv")
-df1 = order_data.sort_VOBCID_FaultCount(df, 300, [1,2,3])
-data1 = data_gen_fc_as_size(df1,"LocationName", "VOBCID", 5000)
+df.index = pd.to_datetime(df.index)
+
+# df1 = order_data.sort_VOBCID_FaultCount(df, 300, [1,2,3])
+# data1 = data_gen_fc_as_size(df1,"LocationName", "VOBCID", 5000)
 
 
 
 app = dash.Dash()
 
 checkboxdict = dict_gen.dict_gen("FaultName", "Fault Code")
-val = []
-#for x in checkboxdict:
-#    val.append()
+
 
 app.layout = html.Div([
     html.Div([    
@@ -61,37 +64,37 @@ app.layout = html.Div([
                 id = 'Checklist',
                 options= checkboxdict,
                 value=  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-                style={'float': 'right', "height" : "80vh",'width': "20vw"}, 
+                style={'float': 'right', "height" : "40vh",'width': "20vw"}, 
                 labelStyle={'display': 'block'}
             )
-        ], className = "six columns"), 
-    ],className="row")    
+        ], className = "six columns"),
+
+        html.Div([  
+        dcc.DatePickerRange(
+            id='date-range',
+            min_date_allowed=df.index.min(),
+            max_date_allowed=df.index.max(),
+            initial_visible_month=df.index.min(),
+            end_date=df.index.max(),
+            #style={'float': 'right', "height" : "20vh",'width': "20vw"}, 
+            #labelStyle={'display': 'block'}
+        )], className = "six columns")
+
+    ],className="row"),
+
+    
+
 ])
-
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
-
-# app.layout = html.Div([S
-#     html.Div([
-#         html.Div([
-#             html.H3('Column 1'),
-#             dcc.Graph(id='g1', figure={'data': [{'y': [1, 2, 3]}]})
-#         ], className="six columns"),
-
-#         html.Div([
-#             html.H3('Column 2'),
-#             dcc.Graph(id='g2', figure={'data': [{'y': [1, 2, 3]}]})
-#         ], className="six columns"),
-#     ], className="row")
-# ])
 
 
 @app.callback(Output('Scatterplot', 'figure'),
-                [Input('Checklist', 'value')])
-def update_Scatter(faultcode_):
-
-    df1 = order_data.sort_VOBCID_FaultCount(df, 300, faultcode_)
+                [Input('Checklist', 'value'),
+                Input('date-range', 'start_date'),
+                Input('date-range', 'end_date')])
+def update_Scatter(faultcode_,start_date ,end_date):
+    mask = (df.index > start_date) & (df.index <= end_date)
+    df1 = df.loc[mask]
+    df1 = order_data.sort_VOBCID_FaultCount(df1, 300, faultcode_)
     data_1 = [data_gen_fc_as_size(df1,"LocationName", "VOBCID", 5000)]
     
     return{'data': data_1, 
